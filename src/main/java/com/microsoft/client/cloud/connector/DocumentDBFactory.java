983b6ea5630f3d94f0cf593c862d97cbd05add47
Kaushik Bhattacharya <kbhatt@microsoft.com>
2016-06-21 10:27:14 +0200

    refactoring

--- src/main/java/com/microsoft/client/cloud/connector/DocumentDBFactory.java
+++ src/main/java/com/microsoft/client/cloud/connector/DocumentDBFactory.java
@@ -1,6 +1,7 @@
 package com.microsoft.client.cloud.connector;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.json.JSONException;
 import org.springframework.beans.BeansException;
@@ -11,23 +12,33 @@ import org.springframework.stereotype.Service;
 
 import com.microsoft.azure.documentdb.ConnectionPolicy;
 import com.microsoft.azure.documentdb.ConsistencyLevel;
+import com.microsoft.azure.documentdb.Database;
 import com.microsoft.azure.documentdb.DocumentClient;
 import com.microsoft.config.Config;
 import com.microsoft.config.DocumentDBProps;
 
 @Service
-public class DocumentClientFactory {
+public class DocumentDBFactory {
 
 	@Autowired
 	private ApplicationContext ctx;
 
 	@Bean
 	public DocumentClient getDocumentClient() throws BeansException, JSONException, IOException {
-		DocumentDBProps props = null;
-		props = ctx.getBean(Config.class).getDocumentDBProps();
+		DocumentDBProps props = ctx.getBean(Config.class).getDocumentDBProps();
 		DocumentClient docDbClient = new DocumentClient(props.getDocumentdb_host(), props.getDocumentdb_key(),
 				ConnectionPolicy.GetDefault(), ConsistencyLevel.Session);
 
 		return docDbClient;
+	}
+
+	@Bean
+	public Database getDocumentDB() throws BeansException, JSONException, IOException {
+		DocumentDBProps props = ctx.getBean(Config.class).getDocumentDBProps();
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
+		List<Database> databases = client
+				.queryDatabases("SELECT * FROM root r WHERE r.id='" + props.getDocumentdb_database() + "'", null)
+				.getQueryIterable().toList();
+		return databases.size() > 0 ? databases.get(0) : null;
 	}
 }
--- src/main/java/com/microsoft/data/access/DocDBDAO.java
+++ src/main/java/com/microsoft/data/access/DocDBDAO.java
@@ -16,11 +16,9 @@ import com.microsoft.azure.documentdb.Document;
 import com.microsoft.azure.documentdb.DocumentClient;
 import com.microsoft.azure.documentdb.DocumentClientException;
 import com.microsoft.azure.documentdb.DocumentCollection;
+import com.microsoft.azure.documentdb.RequestOptions;
 import com.microsoft.azure.documentdb.ResourceResponse;
-import com.microsoft.client.cloud.connector.DocumentClientFactory;
-import com.microsoft.config.Config;
-import com.microsoft.config.DocumentDBAccess;
-import com.microsoft.config.DocumentDBProps;
+import com.microsoft.client.cloud.connector.DocumentDBFactory;
 import com.microsoft.model.SampleModel;
 
 @Service("docDbDao")
@@ -30,30 +28,25 @@ public class DocDBDAO {
 	private ApplicationContext ctx;
 
 	public String save(SampleModel data) throws JSONException, IOException, DocumentClientException {
-		Config config = ctx.getBean(Config.class);
-		DocumentDBAccess dbAccess = ctx.getBean(DocumentDBAccess.class);
-		DocumentClient client = ctx.getBean(DocumentClientFactory.class).getDocumentClient();
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
 
-		DocumentDBProps props = config.getDocumentDBProps();
-
-		Database docDB = dbAccess.getDocumentDB(props.getDocumentdb_database());
+		Database docDB = ctx.getBean(DocumentDBFactory.class).getDocumentDB();
 
 		Document docDbDoc = new Document(new Gson().toJson(data));
 		docDbDoc.set("entityType", data.getType());
 
-		DocumentCollection collection = dbAccess.createDocumentCollection(docDB.getSelfLink(),
-				UUID.randomUUID().toString());
+		DocumentCollection collection = createDocumentCollection(docDB.getSelfLink(), UUID.randomUUID().toString());
 		ResourceResponse<Document> doc = client.createDocument(collection.getSelfLink(), docDbDoc, null, true);
 		return String.valueOf(doc.getStatusCode());
 	}
 
 	public List<DocumentCollection> loadCollections(String databaseLink)
 			throws BeansException, JSONException, IOException {
-		DocumentClient client = ctx.getBean(DocumentClientFactory.class).getDocumentClient();
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
 
 		// Retrieve the collections
 		List<DocumentCollection> collections = client
-				.queryCollections(ctx.getBean(DocumentDBAccess.class).getDocumentDB(databaseLink).getSelfLink(),
+				.queryCollections(ctx.getBean(DocumentDBFactory.class).getDocumentDB().getSelfLink(),
 						"SELECT * FROM root", null)
 				.getQueryIterable().toList();
 
@@ -61,11 +54,35 @@ public class DocDBDAO {
 	}
 
 	public List<Document> loadDocuments(String collectionLink) throws BeansException, JSONException, IOException {
-		DocumentClient client = ctx.getBean(DocumentClientFactory.class).getDocumentClient();
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
 		// Retrieve the TodoItem documents
 		List<Document> docs = client.queryDocuments(collectionLink, "SELECT * FROM root r", null).getQueryIterable()
 				.toList();
 		return docs;
+	}
+
+	public DocumentCollection createDocumentCollection(String databaseLink, String collectionId)
+			throws DocumentClientException, BeansException, JSONException, IOException {
+		DocumentCollection collectionDefinition = new DocumentCollection();
+		collectionDefinition.setId(collectionId);
+		RequestOptions requestOptions = new RequestOptions();
+		requestOptions.setOfferThroughput(1000);
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
+
+		DocumentCollection collection = client.createCollection(databaseLink, collectionDefinition, requestOptions)
+				.getResource();
+		return collection;
+	}
+
+	public DocumentCollection getDocumentDBCollection(String dabaseLink, String collectionId)
+			throws BeansException, JSONException, IOException {
+
+		DocumentClient client = ctx.getBean(DocumentDBFactory.class).getDocumentClient();
+		List<DocumentCollection> collectionList = client
+				.queryCollections(dabaseLink, "SELECT * FROM root r WHERE r.id='" + collectionId + "'", null)
+				.getQueryIterable().toList();
+
+		return collectionList.size() > 0 ? collectionList.get(0) : null;
 	}
 
 }
